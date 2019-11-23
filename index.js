792338814bf0501f22ff55075f8b9712c1af7d1d
favorable-principle-2048 <musta7188@hotmail.it>
2019-11-23 17:46:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(){
-return kittens.push("kitten")
-  
+ kittens.push("kitten")
+
+  return kittens
 }
