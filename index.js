8ee07ef69db5c4bb78c9985230a55a94e5ea6690
favorable-principle-2048 <musta7188@hotmail.it>
2019-11-23 17:49:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name){
- kittens.push("kitten")
+ var kitten2 = kittens.push("kitten")
 
-  return kittens
+  return kittens2
 }
 destructivelyAppendKitten(kittens)
